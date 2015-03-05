248fd0712c2900c6b84664a95e70c9c5180082c8
GamingX <gamingx2005@gmail.com>
2015-03-05 13:02:41 -0600

    Initial commit

---
+++ Debug/hello.pp
@@ -0,0 +1,19 @@
+# FIXED
+
+hello.obj: ../hello.c
+hello.obj: /home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h
+hello.obj: /home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h
+hello.obj: /home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h
+hello.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430.h
+hello.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430f5529.h
+hello.obj: /home/syed/CCS/ccsv5/ccs_base/msp430/include/in430.h
+hello.obj: /home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../hello.c: 
+/home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/stdio.h: 
+/home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/linkage.h: 
+/home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/stdarg.h: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430.h: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/msp430f5529.h: 
+/home/syed/CCS/ccsv5/ccs_base/msp430/include/in430.h: 
+/home/syed/CCS/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
